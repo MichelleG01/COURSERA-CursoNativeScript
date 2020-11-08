e3b7721564d8fd1d41ec9616b74ef91b6d3091bb
Michelle <michelly-gomez@hotmail.com>
2020-11-07 20:08:19 -0500

    Archivos personalizados

---
+++ my-drawer-ng/src/app/share/share.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+import * as app from "@nativescript/core/application";
+
+@Component({
+  selector: 'ns-share',
+  templateUrl: './share.component.html',
+  styleUrls: ['./share.component.css']
+})
+export class ShareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onDrawerButtonTap(): void {
+    const sideDrawer = <RadSideDrawer>app.getRootView();
+    sideDrawer.showDrawer();
+}
+
+}
