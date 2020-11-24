f05de0716df8d532322bf0d8cce91d6db8b0027d
Michelle <michelly-gomez@hotmail.com>
2020-11-23 21:32:25 -0500

    Usar la cámara

--- my-drawer-ng/src/app/home/home.component.ts
+++ my-drawer-ng/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import { Application } from "@nativescript/core";
+import * as camera from "nativescript-camera";
 
 @Component({
     selector: "Home",
@@ -19,5 +20,25 @@ export class HomeComponent implements OnInit {
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>Application.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    onButtonTap(): void {
+        camera.requestPermissions().then( //Solicitamos permisos
+            function sucess() { //aceptacion por parte del usuario
+                //si acepta tomamos una foto, config las opciones del tamaño
+                const options = { width: 300, height: 300, keepAspectRatio: false, saveToGallery: true};
+                camera.takePicture(options).
+                    then((imageAsset) => {
+                        console.log("Tamaño: " + imageAsset.options.width + "x" + imageAsset.options.height);
+                        console.log("keepAspectRatio: " + imageAsset.options.keepAspectRatio);
+                        console.log("Foto guardada");
+                    }).catch((err) => {
+                        console.log("Error -> " + err.message);
+                    });
+            },
+            function failure() { //no aceptacion por parte del usuario
+                console.log("Permiso de camara no aceptado por el usuario");
+            }
+        );
     }
 }
