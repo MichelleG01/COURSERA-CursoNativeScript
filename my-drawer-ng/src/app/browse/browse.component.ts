eabc627b48f3731d1391078865da1f5956d0c56c
Michelle <michelly-gomez@hotmail.com>
2020-11-23 22:14:52 -0500

    Uso de mapas con google

--- my-drawer-ng/src/app/browse/browse.component.ts
+++ my-drawer-ng/src/app/browse/browse.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import { Application } from "@nativescript/core";
+import { registerElement } from "nativescript-angular"; //maps
+
+registerElement("MapView", () => require("nativescript-google-maps-sdk").MapView);
 
 @Component({
     selector: "Browse",
     templateUrl: "./browse.component.html"
 })
 export class BrowseComponent implements OnInit {
+
+    @ViewChild("MapView") mapView: ElementRef;
 
     constructor() {
         // Use the component constructor to inject providers.
@@ -20,4 +25,10 @@ export class BrowseComponent implements OnInit {
         const sideDrawer = <RadSideDrawer>Application.getRootView();
         sideDrawer.showDrawer();
     }
+
+     // Map events
+    onMapReady(event): void {
+        console.log("Map Ready");
+    }
+    
 }
